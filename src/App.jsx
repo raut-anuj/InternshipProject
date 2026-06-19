357f2db506b06ebf6cee8bfa40be50675dcd9f64
Anuj Raut <personeluse2000@example.com>
2026-06-19 11:39:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-600">
+        Tailwind Working.
+      </h1>
+    </div>
+  )
+}
