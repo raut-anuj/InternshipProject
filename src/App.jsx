604444b29d9c0b8a8eaf25892f6da964f39e1167
Anuj Raut <personeluse2000@example.com>
2026-06-24 10:56:33 +0530

    feat: uncomment component imports and update CardStackCarousel usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-// import User from "./components/User"
-//  import DigiCard from "./components/DigiCard"
-// import LogoSlider from "./components/LogoSlider"
-// import Animation  from "./pages/Animation";
-// import Carousel from "./components/Carousel";
-// import NameSlider from "./components/NameSlider";
-// import Slider from "./components/Slider";
-// import FadingLogoSlider from "./components/FadingLogoSlider";
-// import BrandSlider from "./components/BrandSlider";
-// import AdvancedDualSlider from "./components/AdvancedDualSlider";
-// import GovtStyleSlider from "./components/GovtStyleSlider";
+import User from "./components/User"
+import DigiCard from "./components/DigiCard"
+import LogoSlider from "./components/LogoSlider"
+import Animation  from "./pages/Animation";
+import Carousel from "./components/Carousel";
+import NameSlider from "./components/NameSlider";
+import Slider from "./components/Slider";
+import FadingLogoSlider from "./components/FadingLogoSlider";
+import BrandSlider from "./components/BrandSlider";
+import AdvancedDualSlider from "./components/AdvancedDualSlider";
+import GovtStyleSlider from "./components/GovtStyleSlider";
 
 import CardStackCarousel from "./components/CardStackCarousel";
 import SpotlightBrandSlider from "./components/SpotlightBrandSlider";
@@ -19,7 +19,7 @@ function App() {
   return (
     <div>
           {/* {<SpotlightBrandSlider  /> } */}
-          {<CardStackCarousel items={data} />}
+          {/* {<CardStackCarousel items={data} />} */}
 
           {/* {<AdvancedDualSlider />} */}
           {/* {<FadingLogoSlider items={data}/>} */}
