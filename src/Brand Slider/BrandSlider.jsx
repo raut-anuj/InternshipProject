4d74201d35efb8245a55871b56f85aa4f883d05b
Niraj Raut <84171890+rautniraj@users.noreply.github.com>
2026-07-09 18:20:28 +0530

    Refactor BrandSlider to use imported logos

--- src/Brand Slider/BrandSlider.jsx
+++ src/Brand Slider/BrandSlider.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import { Image } from "react-bootstrap";
 
-export default function BrandSlider() {
-  const logos = [
-  { src: "/image/deloitte.png" },
-  { src: "/image/icici.png" },
-  { src: "/image/johnson.png" },
-  { src: "/image/mahindra.png" },
-  { src: "/image/mercede.png" },  
-  { src: "/image/paper.png" },
-  { src: "/image/union.png" },
-];
+import deloitteLogo from "../../../assets/images/clients_logo/deloitte.png";
+import iciciLogo from "../../../assets/images/clients_logo/icici.png";
+import johnsonLogo from "../../../assets/images/clients_logo/johnson.png";
+import mahindraLogo from "../../../assets/images/clients_logo/mahindra.png";
+import mercedeLogo from "../../../assets/images/clients_logo/mercede.png";
+import paperLogo from "../../../assets/images/clients_logo/paper.png";
+import unionLogo from "../../../assets/images/clients_logo/union.png";
 
-const renderNameCard = ({ src, key }) => (
-  <div className="logo-card" key={key}>
-    <Image className="logo-img" src={src} draggable={false} />
-  </div>
-);
+export default function BrandsSlider() {
+  const logos = [
+    { src: deloitteLogo },
+    { src: iciciLogo },
+    { src: johnsonLogo },
+    { src: mahindraLogo },
+    { src: mercedeLogo },
+    { src: paperLogo },
+    { src: unionLogo },
+  ];
+
+  const renderNameCard = ({ src, key }) => (
+    <div className="logo-card" key={key}>
+      <Image className="logo-img" src={src} draggable={false} />
+    </div>
+  );
 
   return (
-    
-    <div className="photo-slider">
+    <div className="brands-slider">
       <section className="slider-section">
-        <h2 className="text-style">Trusted by Leading Brands</h2>
+        <h2 className="text-style">Trusted by Organizations</h2>
         <div className="separator"></div>
-
         <div className="slider-fade">
-          <div className="slider-fade marquee-track">
-            <div className="logo">
+          <div className="marquee-track">
+            <div className="logo-set">
               {logos.map((item, index) =>
                 renderNameCard({
                   src: item.src,
@@ -36,7 +42,7 @@ const renderNameCard = ({ src, key }) => (
               )}
             </div>
 
-            <div className="logo">
+            <div className="logo-set">
               {logos.map((item, index) =>
                 renderNameCard({
                   src: item.src,
