d1473ea05a1e161eb2749b6a85184b82f7429d2b
Anuj Raut <personeluse2000@gmail.com>
2026-07-09 17:49:29 +0530

    feat: Add BrandSlider component and remove NameCard component

--- src/Brand Slider/BrandSlider.jsx
+++ src/Brand Slider/BrandSlider.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import NameCard from "./NameCard";
- 
-const logos = [
+import { Image } from "react-bootstrap";
+
+export default function BrandSlider() {
+  const logos = [
   { src: "/image/deloitte.png" },
   { src: "/image/icici.png" },
   { src: "/image/johnson.png" },
   { src: "/image/mahindra.png" },
-  { src: "/image/mercede.png" },
+  { src: "/image/mercede.png" },  
   { src: "/image/paper.png" },
   { src: "/image/union.png" },
 ];
 
-export default function PhotoSlider() {
+const renderNameCard = ({ src, key }) => (
+  <div className="logo-card" key={key}>
+    <Image className="logo-img" src={src} draggable={false} />
+  </div>
+);
+
   return (
+    
     <div className="photo-slider">
       <section className="slider-section">
         <h2 className="text-style">Trusted by Leading Brands</h2>
@@ -21,15 +28,21 @@ export default function PhotoSlider() {
         <div className="slider-fade">
           <div className="slider-fade marquee-track">
             <div className="logo">
-              {logos.map((item, index) => (
-                <NameCard key={`set1-${index}`} src={item.src} />
-              ))}
+              {logos.map((item, index) =>
+                renderNameCard({
+                  src: item.src,
+                  key: `set1-${index}`,
+                }),
+              )}
             </div>
 
             <div className="logo">
-              {logos.map((item, index) => (
-                <NameCard key={`set2-${index}`} src={item.src} />
-              ))}
+              {logos.map((item, index) =>
+                renderNameCard({
+                  src: item.src,
+                  key: `set1-${index}`,
+                }),
+              )}
             </div>
           </div>
         </div>
--- src/Brand Slider/NameCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NameCard({ src }) {
-  return (
-    <div className="logo-card">
-      <img className="logo-img" src={src} draggable={false} />
-    </div>
-  );
-}
