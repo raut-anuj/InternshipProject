a93be8a8b1afc14bbc8803237165a8dd0a4149b8
Niraj Raut <84171890+rautniraj@users.noreply.github.com>
2026-08-05 09:01:52 +0530

    Refactor WebsiteStats component and loading logic
    Refactored WebsiteStats component to improve loading state handling and removed unnecessary code. Updated StatsCard rendering logic for better clarity.

--- src/Website Stats/WebsiteStats.jsx
+++ src/Website Stats/WebsiteStats.jsx
@@ -9,107 +9,107 @@ import {
   faPeopleLine,
   faSitemap,
   faUsers,
-  faVideo,
   faHandshake,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function WebsiteStats() {
-  
+
   // Flexible from 1 to N
   const numberOfCards = 6;
 
   const { data: websiteStatsData, isLoading: websiteStatsLoading } = useWebsiteStats();
 
-  if (websiteStatsLoading) {
-    return (
-      <div className="website-stats">
-        <Row className="holder-row placeholder-row justify-content-center">
-          {Array.from({ length: numberOfCards }).map((_, index) => (
-            <Col
-              key={`stats-placeholder-${index}`}
-              xs="auto"
-              className="placeholder-col d-flex justify-content-center"
-            >
-              <StatsCardPlaceholder />
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
-
-  const getVal = (prop) =>
-    websiteStatsData?.find((item) => item.property === prop);
+  const getVal = (prop) => websiteStatsData?.find((item) => item.property === prop);
 
   return (
     <div className="website-stats">
-      <Row className="website-stats holder-row placeholder-row justify-content-center">
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Platform Users"
-            obj={getVal("active_user_count")}
-            icon={faUsers}
-          />
-        </Col>
+      <h2 className="text-style">Platform Highlights</h2>
+      <div className="separator"></div>
 
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Ministries"
-            obj={getVal("ministry_count")}
-            icon={faLandmark}
-          />
-        </Col>
+      {websiteStatsLoading ?
+        (
+          <Row className="holder-row placeholder-row justify-content-center">
+            {Array.from({ length: numberOfCards }).map((_, index) => (
+              <Col
+                key={`stats-placeholder-${index}`}
+                xs="auto"
+                className="placeholder-col d-flex justify-content-center"
+              >
+                <StatsCardPlaceholder />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Row className="website-stats holder-row placeholder-row justify-content-center">
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Platform Users"
+                obj={getVal("active_user_count")}
+                icon={faUsers}
+              />
+            </Col>
 
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Organizations"
-            obj={getVal("organization_count")}
-            icon={faSitemap}
-          />
-        </Col>
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Ministries"
+                obj={getVal("ministry_count")}
+                icon={faLandmark}
+              />
+            </Col>
 
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Meetings Hosted"
-            obj={getVal("total_meetings")}
-            icon={faHandshake}
-          />
-        </Col>
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Organizations"
+                obj={getVal("organization_count")}
+                icon={faSitemap}
+              />
+            </Col>
 
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Conference Hours"
-            obj={getVal("total_duration")}
-            icon={faClock}
-          />
-        </Col>
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Meetings Hosted"
+                obj={getVal("total_meetings")}
+                icon={faHandshake}
+              />
+            </Col>
 
-        <Col
-          xs="auto"
-          className="placeholder-col d-flex justify-content-center"
-        >
-          <StatsCard
-            title="Participants Connected"
-            obj={getVal("meeting_attendees_count")}
-            icon={faPeopleLine}
-          />
-        </Col>
-      </Row>
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Conference Hours"
+                obj={getVal("total_duration")}
+                icon={faClock}
+              />
+            </Col>
+
+            <Col
+              xs="auto"
+              className="placeholder-col d-flex justify-content-center"
+            >
+              <StatsCard
+                title="Participants Connected"
+                obj={getVal("meeting_attendees_count")}
+                icon={faPeopleLine}
+              />
+            </Col>
+          </Row>
+        )
+      }
+
     </div>
   );
 }
