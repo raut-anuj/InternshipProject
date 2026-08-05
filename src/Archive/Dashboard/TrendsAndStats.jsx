6aadeddf73f81972c71d157acdfda7d32cc4c2d9
Niraj Raut <84171890+rautniraj@users.noreply.github.com>
2026-08-05 16:08:12 +0530

    Rename TrendsAndStats component to TrendAndStats

--- src/Archive/Dashboard/TrendsAndStats.jsx
+++ src/Archive/Dashboard/TrendsAndStats.jsx
@@ -3,16 +3,21 @@ import ListExample from "../Nav/ListExample"
 import StatsTab from "../UI for Charts/StatsTab"
 import "./TrendsAndStats.css"
 
-export default function TrendsAndStats() {
+export default function TrendAndStats() {
   return (
-    <div className="dashboard-container">
-  <div className="chart-section">
-    <ListExample />
-  </div>
+    <div>
+      <h2 className="text-style">Platform Highlights</h2>
+      <div className="separator"></div>
 
-  <div className="stats-section">
-    <StatsTab />
-  </div>
-</div>
+      <div className="dashboard-container">
+        <div className="chart-section">
+          <UsageTrend />
+        </div>
+
+        <div className="stats-section">
+          <StatsTab />
+        </div>
+      </div>
+    </div>
   );
 }
